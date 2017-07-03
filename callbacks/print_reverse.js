9d4ae1e25094c8a678164d9f1f96297e091ef6fd
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-03 19:37:55 +0000

    wrote callback function for getHTML to reverse output

---
+++ callbacks/print_reverse.js
@@ -0,0 +1,15 @@
+//reverses the html string and
+//prints to console
+
+var getHTML = require("../http-functions");
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/reverse.html'
+};
+
+function printReverse(html){
+  return getHTML.reverse();
+}
+
+getHTML(requestOptions, printReverse);
