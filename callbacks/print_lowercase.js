d71c412260e6c129d3ec77203cc8d6bd74b79193
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-03 19:35:19 +0000

    created new function to print to all lowercase using getHTML module

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,15 @@
+//transforms html string into lowercase and
+//prints to console
+
+var getHTML = require("../http-functions");
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printLowercase(html){
+  return getHTML.toLowerCase();
+}
+
+getHTML(requestOptions, printLowercase);
