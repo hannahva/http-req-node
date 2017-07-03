eb87c911a46f1ed2f3439c583211da70da47f725
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-03 18:07:25 +0000

    added callback function for printing the html, called in the get html function

---
+++ get-HTML-callback.js
@@ -0,0 +1,30 @@
+var https = require("https");
+var buffer ="";
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+function getHTML(options, callback){
+
+  https.get(options, function(response){
+    response.setEncoding("utf8");
+
+    response.on("data", function(data){
+      buffer += data.toString();
+      printHTML(buffer);
+    })
+
+    response.on("end", function(){
+      console.log("Response stream complete.");
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML(requestOptions, printHTML());
